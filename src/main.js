2c02d2053c7a8e7d27ef5cc211037a0b8f46675a
Ram Joshi <ram@ramjoshi.com>
2012-03-02 15:13:11 -0800

    Added date formatting and fixed whitespace wrapping of rows.

--- src/main.js
+++ src/main.js
@@ -1,24 +1,28 @@
 $(function() {
-    var teams = ['milan', 'juve']; // declare team names to match names of respective csv files.
+    var teams = ['juve', 'milan']; // declare team names to match names of respective csv files.
 
     // draw team data inside element.
     var drawTable = function(team, element) {
         data = 'assets/data/' + team + '-goals.csv';
         d3.text(data, function(datasetText) {
         var csv = d3.csv.parseRows(datasetText);
+        var header = csv.splice(0, 1);
         csv.forEach(function(o) {
-            o[7] = parseInt(o[7]);
             o[0] = parseInt(o[0]);
-            o[2]=Date.parse(o[2]);
-            csv[team+'Goal'+o] = csv.pop();
+            o[2] = Date.parse(o[2]);
+            o[7] = parseInt(o[7]);
         });
+        csv = header.concat(csv);
         d3.select(element).selectAll('ul') // each row is an unsorted list
             .data(csv)
             .enter().append('ul')
             .html(function(d) {
                 var row = $('<ul>');
                 for(e in d) {
-                  row.append($('<li>').text(d[e])); // each data point is a list element
+                    if(e==2) {
+                        d[e] = d[e].toString('MMM-d-yyyy');
+                    }
+                    row.append($('<li>').text(d[e])); // each data point is a list element
                 }
                 return row.html();
             });
