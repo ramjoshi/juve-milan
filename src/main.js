08b29a18411407ad6eb31fc70347f7143ff810cc
Rami Taibah <taibah@gmail.com>
2012-03-05 21:45:15 -0800

    Added second team. Now they are opposite, one straight up another upside down. TODO: add 0 goal-years

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
-$(function() {
+
+
+$(function(){
+
+    
     var teams = ['juve', 'milan']; // declare team names to match names of respective csv files.
     var years = [];
     var yearly = {};
-    var loadData = function(team) {
+
+    function  loadData(team) {
         data = 'assets/data/' + team + '-goals.csv';
         d3.text(data, function(datasetText) {
             var csv = d3.csv.parseRows(datasetText);
@@ -22,10 +27,12 @@ $(function() {
                 years.push(year);
                 yearly[year] = minute;
             });
-            var svg = d3.select("#table")
+	    console.log(csv);	
+	    if (team==='juve'){
+	    var svg = d3.select("#table")
                 .append("svg")
-                .attr("width", 2000)
-                .attr("height", 800);
+                .attr("width", 1000)
+                .attr("height", 400);
             svg.selectAll("rect")
                    .data(years)
                    .enter()
@@ -38,7 +45,7 @@ $(function() {
                        for(m in yearly[d]) {
                            goals += 1;
                        }
-                       return 800 - goals*20;
+                       return 400 - goals*20;
                    })
                    .attr("width", 5)
                    .attr("height", function(d, i) {
@@ -47,10 +54,45 @@ $(function() {
                            goals += 1;
                        }
                        if(goals==9){console.log(d);}
-                       return goals*20;
-                   })
-        });
-    }
+                      return goals*20;
+		       })
+		}
 
-    loadData('juve');
-});
+		   else{
+		        var svg2 = d3.select("#table")
+                		.append("svg")
+		                .attr("width", 1000)
+        		        .attr("height", 400);
+            		svg2.selectAll("rect")
+                	   .data(years)
+	                   .enter()
+        	           .append("rect")
+                	   .attr("x", function(d, i) {
+                    	   return i*10;
+                  		 })
+                           .attr("y", function(d, i) {
+   	                    var goals = 0;
+        	            for(m in yearly[d]) {
+                	           goals += 1;
+                       }
+                       //return 400 - goals*20;
+                   })
+                   .attr("width", 5)
+		   .style("fill","red")		   
+                   .attr("height", function(d, i) {
+                       var goals = 0;
+                       for(m in yearly[d]) {
+                           goals += 1;
+                       }
+                       if(goals==9){console.log(d);}
+                      return goals*20;
+		   }
+		   )
+		 }
+	    }
+        )};
+     for (i=0;i<2;++i){loadData(teams[i]);}  
+
+}
+
+);
