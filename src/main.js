f88adb032ee5d7bff0e7c90e097132a8f7474e28
Ram Joshi <ram@ramjoshi.com>
2012-03-06 15:19:47 -0800

    Modified auto bar positions and width

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 $(function() {
+    var data = {};
     var columns = ['pos', 'comp', 'date', 'match', 'score', 'goalscorer', 'goalkeeper', 'min', 'part']; // csv data columns
 
     /*
@@ -6,18 +7,21 @@ $(function() {
      */
     var loadData = function(team, callback) {
         var csv = 'assets/data/' + team + '-goals.csv';
-        var data;
-        d3.text(csv, function(datasetText) {
-            data = d3.csv.parseRows(datasetText);
-            var header = data.splice(0, 1);
-            data.forEach(function(row) {
-                // there must be a less verbose way of applying these functions
-                row[columnIndex('pos')] = parseInt(row[columnIndex('pos')]);
-                row[columnIndex('date')] = Date.parse(row[columnIndex('date')]);
-                row[columnIndex('min')] = parseInt(row[columnIndex('min')]);
+        if(data[team]) callback(data[team]); // check cache
+        else {
+            d3.text(csv, function(datasetText) {
+                var dt = d3.csv.parseRows(datasetText);
+                var header = dt.splice(0, 1);
+                dt.forEach(function(row) {
+                    // there must be a less verbose way of applying these functions
+                    row[columnIndex('pos')] = parseInt(row[columnIndex('pos')]);
+                    row[columnIndex('date')] = Date.parse(row[columnIndex('date')]);
+                    row[columnIndex('min')] = parseInt(row[columnIndex('min')]);
+                });
+                data[team] = dt; // cache data
+                callback(dt);
             });
-            callback(data);
-        });
+        }
     }
 
     // Returns the index for a column name
@@ -45,7 +49,7 @@ $(function() {
         },
         minutelyData: function (data, from, to) {
             return _.groupBy(data, function(row) {
-                return row[columnIndex('min')];
+                return 10*(Math.floor(row[columnIndex('min')]/10)+1);
             });
         },
         maxGoals: function (data) {
@@ -55,28 +59,26 @@ $(function() {
         }
     });
 
-    function updateMinutelyGraph(mindata, maxy, align, from, to) {
-        from = from || 0;
-        to = (to+1) || 101;
-        var svg = d3.select("#graph svg");
+    function updateMinutelyGraph(mindata, maxy, align) {
+        var svg = d3.select('#graph svg');
         var width = $('#graph svg').width();
-        var barwidth = width/(3*(2*(to-from)-1));
+        var xunit = width/88;
         var height = $('#graph svg').height();
         var barheight_factor = height*0.9/maxy;
         svg.selectAll('rect.'+align)
-            .data(_.range(from, to))
+            .data(_.range(10, 110, 10))
             .enter()
-            .append("rect").attr('class', align)
-            .attr("x", function(d, i) {
-                if(align=='left') return 6*i*barwidth;
-                else return (2+6*i)*barwidth;
+            .append('rect').attr('class', align)
+            .attr('x', function(d, i) {
+                if(align=='left') return 9*i*xunit;
+                else return (9*i-5)*xunit;
             })
-            .attr("y", function(d, i) {
+            .attr('y', function(d, i) {
                 if(mindata[d]) return height - mindata[d].length*barheight_factor;
                 return height;
             })
-            .attr("width", barwidth)
-            .attr("height", function(d, i) {
+            .attr('width', 4*xunit)
+            .attr('height', function(d, i) {
                 if(mindata[d]) return mindata[d].length*barheight_factor;
                 else return 0;
             });
@@ -95,12 +97,14 @@ $(function() {
     function initCompare(teamA, teamB, fromYear, toYear, fromMin, toMin) {
         fromYear = fromYear || 1900;
         toYear = toYear || 2000;
-        fromMin = fromMin || 0;
+        fromMin = fromMin || 0
         toMin = toMin || 100;
         initMinutelyData(teamA, fromYear, toYear, fromMin, toMin, 
                 function(dA) {
                     initMinutelyData(teamB, fromYear, toYear, fromMin, toMin, 
                         function(dB) {
+                            console.log(dA);
+                            console.log(dB);
                             var maxy = Math.max(_.maxGoals(dA), _.maxGoals(dB));
                             updateMinutelyGraph(dA, maxy, 'left', fromMin, toMin);
                             updateMinutelyGraph(dB, maxy, 'right', fromMin, toMin);
@@ -108,5 +112,5 @@ $(function() {
                 });
     }
 
-    initCompare('juve', 'milan', 1900, 2000, 10, 50);
+    initCompare('juve', 'milan');
 });
